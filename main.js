b0f085d3db9bda3cd5033f027b81ad3a49a07d70
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-03-04 21:45:55 +0530

    ...

--- main.js
+++ main.js
@@ -4,12 +4,7 @@ var keys = [];
 
 cameraMove = {x:0, z:0}
 
-var url = "https://cdn.rawgit.com/BabylonJS/Extensions/master/DynamicTerrain/dist/babylon.dynamicTerrain.min.js";
-var s = document.createElement("script");
-s.src = url;
-document.head.appendChild(s);
-
-var light, camera, waterMesh, ground
+var light, camera, waterMesh, skybox
 
 var createScene = function() {
     var scene = new BABYLON.Scene(engine);
@@ -24,10 +19,10 @@ var createScene = function() {
 
 	// light1
 	light = new BABYLON.SpotLight("spotLight", new BABYLON.Vector3(-0, 80, -80), new BABYLON.Vector3(0, -1, 0), Math.PI / 5, 1, scene);
-	light.intensity = 1;
+	light.intensity = 0.8;
 
 	// Skybox
-	var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+	skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
 	skyboxMaterial.backFaceCulling = false;
 	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/sky", scene);
@@ -46,7 +41,7 @@ var createScene = function() {
 	shadowGenerator.useKernelBlur = true;
 	shadowGenerator.blurKernel = 32;
 	shadowGenerator.useContactHardeningShadow = true;
-	shadowGenerator.setDarkness(0.01);
+	shadowGenerator.setDarkness(0.05);
 
 	// Ground
 	var groundTexture = new BABYLON.Texture("textures/sand.jpg", scene);
@@ -54,34 +49,69 @@ var createScene = function() {
 	
 	var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
 	groundMaterial.diffuseTexture = groundTexture;
-	
-	ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
-	ground.position.y = -1;
-	ground.material = groundMaterial;
+	// set shininess to a small value (between 0 and 1) to avoid the ground to look shiny
+	groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
 
 	// Water
 	waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, scene, false);
 	var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(1000, 1000));
-	water.backFaceCulling = true;
+	water.backFaceCulling = false;
 	water.bumpTexture = new BABYLON.Texture("textures/waterbump.png", scene);
 	water.windForce = -8;
-	water.waveHeight = 0.8;
-	water.bumpHeight = 0.8;
-	water.waveLength = 3;
-	water.colorBlendFactor = 0.4;
+	water.waveHeight = 0.005;
+	water.bumpHeight = 0.05;
+	water.waveLength = 0.1;
+	water.waveSpeed = 0.846;
+	water.colorBlendFactor = 0.3;
 	water.addToRenderList(skybox);
-	water.addToRenderList(ground);
 	waterMesh.material = water;
-	
-	var b = BABYLON.Mesh.CreateBox("box", 1, scene, false);
-	b.scaling.y = 20;
-	b.position.y = 10;
-	b.position.x = -10;
-	b.position.z = -10;
 
-	shadowGenerator.addShadowCaster(b);
-	ground.receiveShadows = true;
-	waterMesh.receiveShadows = true;
+	var mapSubX = 500;
+	var mapSubZ = 300;
+	var terrainSub = 100;
+
+	// map creation
+	var mapSubX = 1000;             // point number on X axis
+    var mapSubZ = 800;              // point number on Z axis
+    var seed = 0.3;                 // seed
+    var noiseScale = 0.01;         // noise frequency
+    var elevationScale = 20;
+    noise.seed(seed);
+    var mapData = new Float32Array(mapSubX * mapSubZ * 3); // 3 float values per point : x, y and z
+
+    var paths = [];                             // array for the ribbon model
+    for (var l = 0; l < mapSubZ; l++) {
+        var path = [];                          // only for the ribbon
+        for (var w = 0; w < mapSubX; w++) {
+            var x = (w - mapSubX * 0.5) * 2.0;
+            var z = (l - mapSubZ * 0.5) * 2.0;
+            var y = Math.abs(noise.simplex2(x * noiseScale, z * noiseScale));
+            y *= (0.5 + y) * y * elevationScale;   // let's increase a bit the noise computed altitude
+                   
+            mapData[3 *(l * mapSubX + w)] = x;
+            mapData[3 * (l * mapSubX + w) + 1] = y;
+            mapData[3 * (l * mapSubX + w) + 2] = z;
+            
+            path.push(new BABYLON.Vector3(x, y, z));
+        }
+        paths.push(path);
+    }
+	var params = {
+		mapData: mapData,
+		mapSubX: mapSubX,
+		mapSubZ: mapSubZ,
+		terrainSub: terrainSub
+	};
+	terrain = new BABYLON.DynamicTerrain("terrain", params, scene);
+	terrain.mesh.material = groundMaterial;
+	terrain.subToleranceX = 8;
+	terrain.subToleranceZ = 8;
+	terrain.LODLimits = [4, 3, 2, 1, 1];
+	terrain.mesh.receiveShadows = true;
+	terrain.mesh.checkCollisions = true;
+	terrain.mesh.material.freeze();
+	terrain.mesh.position.y = -2;
+	water.addToRenderList(terrain.mesh);
 
 	return scene;
 };
@@ -121,9 +151,8 @@ engine.runRenderLoop(function () {
 
 	waterMesh.position.x = camera.position.x;
 	waterMesh.position.z = camera.position.z;
-
-	ground.position.x = camera.position.x;
-	ground.position.z = camera.position.z;
+	skybox.position.x = camera.position.x;
+	skybox.position.z = camera.position.z;
 });
 
 // Watch for browser/canvas resize events
